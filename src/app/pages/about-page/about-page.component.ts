7a55bf698a7d2fc067c65059bdd26626d403a831
Juanma Molins Cortés <juan.maria.molins@accenture.com>
2024-10-22 15:43:31 +0200

    remove about page content

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -24,20 +24,11 @@ export class AboutPageComponent implements OnInit {
   public skillData: Skill[] = skillContent;
   // Skeleton loader
   public showSkeleton: boolean = true;
-  // Swiper
-  currentSlide = 0;
-  // Number of slides
-  slides: any[] = []; // Dynamic slides
-  // Scrolling flag
-  isScrolling = false;
-  // Footer visibility
-  isFooterVisible = false;
 
   constructor( private _sharedService: SharedService, private _pagesService: PagesService ) {}
 
   ngOnInit(): void {
     this.darkMode();
-    this.getSlides();
     
     setTimeout(() => {
       this.getSkills();
@@ -58,59 +49,6 @@ export class AboutPageComponent implements OnInit {
       this.skillData = skills;
       this.showSkeleton = false;
     });
-  }
-
-  getSlides() {
-    this._pagesService.getSlides().subscribe((slides: any[]) => {
-      this.slides = slides;
-      this.showSkeleton = false;
-    });
-  }
-
-  @HostListener('window:wheel', ['$event'])
-  onScroll(event: WheelEvent) {
-    if (this.isScrolling) return;
-
-    this.isScrolling = true;
-    if (event.deltaY > 0) {
-      this.nextSlide();
-    } else {
-      this.prevSlide();
-    }
-
-    setTimeout(() => {
-      this.isScrolling = false;
-    }, 800); // Adjust timeout to match the CSS transition duration
-
-    this.checkFooterVisibility();
-  }
-
-  nextSlide() {
-    if (this.currentSlide < this.slides.length - 1) {
-      this.currentSlide++;
-      this.updateSlidePosition();
-    }
-  }
-
-  prevSlide() {
-    if (this.currentSlide > 0) {
-      this.currentSlide--;
-      this.updateSlidePosition();
-    }
-  }
-
-  goToSlide(index: number) {
-    this.currentSlide = index;
-    this.updateSlidePosition();
-  }
-
-  updateSlidePosition() {
-    const swiperWrapper = document.querySelector('.swiper-wrapper') as HTMLElement;
-    swiperWrapper.style.transform = `translateY(-${this.currentSlide * 100}vh)`;
-  }
-
-  checkFooterVisibility() {
-    this.isFooterVisible = (this.currentSlide === this.slides.length - 1);
   }
 
 }
