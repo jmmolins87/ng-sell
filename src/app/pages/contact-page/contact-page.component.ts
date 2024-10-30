74d593f29f39fbcbef67c82dbd6c34244f63014c
Juanma Molins Cortés <juan.maria.molins@accenture.com>
2024-10-30 17:48:31 +0100

    add translate error message in contact page, fixes in navbar and add any colors in bg token

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -5,6 +5,7 @@ import { SharedService } from '../../shared/services/shared.service';
 import { PagesService } from '../../services/pages-service.service';
 
 import { titleTypes } from '../../components/title-pages/title.config';
+import { customEmailValidator } from './email.validator';
 
 @Component({
   selector: 'app-contact-page',
@@ -16,7 +17,7 @@ export class ContactPageComponent {
   // Dark Theme
   public isDarkMode: boolean = false;
   // Show or hide the skeleton loader
-  public showSkeleton: boolean = false;
+  public showSkeleton: boolean = true;
   // Show or hide the form
   public messageCharactres: string = '';
   // Initialize the character count to 0
@@ -25,27 +26,25 @@ export class ContactPageComponent {
   public maxCharacters: number = 1000;
   // Set the title of the page
   public titleType: titleTypes = titleTypes.h1;
-  // Text error fields
-  public textError: string = 'Este campo es requerido';
   // Set the title of the page
   public formContact: FormGroup = this._fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     surname: [''],
     company: ['', [Validators.required, Validators.minLength(5)]],
-    email: ['', [Validators.required, Validators.email]],
+    email: ['', [Validators.required, Validators.email, customEmailValidator()]],
     phone: [''],
     subject: ['', [Validators.required, Validators.minLength(5)]],
     message: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(this.maxCharacters)]]
   });
   
-  constructor ( 
-    private _sharedService: SharedService, 
-    private _fb: FormBuilder,
-    private _validatorsService: PagesService ) { }
+  constructor ( private _sharedService: SharedService, private _fb: FormBuilder ) { }
 
   ngOnInit() {
-    this.resetFormError();
     this.darkMode();
+
+    setTimeout(() => {
+      this.showSkeleton = false;
+    }, 500)
   }
 
   // Function to get the dark mode status
@@ -65,30 +64,12 @@ export class ContactPageComponent {
   get subject() { return this.formContact.get('subject'); }
   get message() { return this.formContact.get('message'); }
 
-  resetFormError() { this.formContact.reset(); }
   updateCounter() { this.characterCount = this.messageCharactres.length; }
 
-  isValidField( field: string ): boolean | null {
-    return this._validatorsService.isValidField(this.formContact, field);
-  }
-
-  getFieldError( field: string ): string | null {
-
-    if(!this.formContact.controls[field]) return null;
-    const errors = this.formContact.controls[field].errors || {};
-    for ( const key of Object.keys( errors ) ) {
-      switch (key) {
-        case 'required':
-          return 'Este campo es requerido';
-          break;
-        case 'minlength':
-          return `Mínimo ${ errors['minlength'].requiredLength } carácteres`;
-          break;
-        default:
-          break;
-      }
-    }
-    return null;
+  resetForm(): void {
+    this.formContact.reset();
+    this.messageCharactres = '';
+    this.characterCount = 0;
   }
 
 }
