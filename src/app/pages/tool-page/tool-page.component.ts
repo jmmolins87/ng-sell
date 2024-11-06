b07ecb6686c450cd6136ab94d3ccdb20ea3c5736
Juanma Molins Cortés <juan.maria.molins@accenture.com>
2024-11-06 16:01:40 +0100

    add styles in contact, init process tool page and add styles in primeNg style sheet

--- src/app/pages/tool-page/tool-page.component.ts
+++ src/app/pages/tool-page/tool-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-tool-page',
   templateUrl: './tool-page.component.html',
   styleUrls: ['./tool-page.component.scss']
 })
-export class ToolPageComponent {
+export class ToolPageComponent implements OnInit {
+
+  // Dark Theme
+  public isDarkMode: boolean = false;
+
+  constructor( private _sharedService: SharedService ) {}
+
+  ngOnInit() {
+    this.darkMode();
+  }
+
+  // Function to get the dark mode status
+  darkMode(): void {
+    // Subscribe to the dark mode status to apply the class
+    this._sharedService.darkMode$.subscribe((isDarkMode) => {
+      this.isDarkMode = isDarkMode;
+    });
+  }
 
 }
